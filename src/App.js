6fa692b032286902e10348d9ad2dde2bee7a9e88
maguiceri <magui.cerisola@gmail.com>
2020-07-11 00:44:14 -0700

    input

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      firstNum:0,
+      secondNum:0,
+      result:0
+    }
+  }
+
+  handleSum() {
+    this.setState({
+      result: parseInt(this.state.firstNum) + parseInt(this.state.secondNum)
+    })
+  }
+
+  handleFirstNum(e) {
+    this.setState({
+      firstNum: e.target.value
+    })
+  }
+
+  handleSecondNum(e) {
+    this.setState({
+      secondNum: e.target.value
+    })
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <input onChange={(e) => this.handleFirstNum(e)} type="text" />
+        <input onChange={(e) => this.handleSecondNum(e)} type="text" />
+        <button onClick={ () => this.handleSum()}>Sumar</button>
+        <div>
+          <p>Resultado: {this.state.result}</p>
+        </div>
+      </React.Fragment>
+
+    )
+  }
 }
-
 export default App;
